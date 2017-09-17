36daeb77b704045fbbf2d3864484b18b6b5f3716
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2017-09-18 07:01:59 +0900

    Bugfix

--- src/usi.cpp
+++ src/usi.cpp
@@ -1627,7 +1627,7 @@ getMoveValuesBySearch(const std::string& teacherFileName, const int batchSize){
 
 PYBIND11_PLUGIN(nndata) {
     py::module m("nndata", "data ganerator for neural network");
-    m.def("get_inputs_moves_values", &getInputsFromSfen,
+    m.def("get_inputs_from_sfen", &getInputsFromSfen,
           "A function which returns inputs by sfen positions");
     m.def("get_inputs_moves_values", &getInputsMovesValues,
           "A function which returns inputs, moves and values");
